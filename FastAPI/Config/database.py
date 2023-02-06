77718b7ab1849b3abdf89d26dd90aa234b70dfae
apkmew <apimook121@gmail.com>
2023-02-06 22:45:17 +0700

    practice

--- FastAPI/Config/database.py
+++ FastAPI/Config/database.py
@@ -1,10 +1,9 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-import urllib
 load_dotenv('.env')
 
 user = os.getenv('user')
-password = urllib.parse.quote(os.getenv('password'))
-client = MongoClient(f'mongodb://{user}:{urllib.parse.quote(password)}@mongo.exceed19.online:8443/?authMechanism=DEFAULT')
+password = os.getenv('password')
+client = MongoClient(f'mongodb://{user}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT')
 db = client['exceed04']
